e1bbbfdd21fafe8dfb241fa0aac47586495f1303
Ian Cavini <iancavinipinzon@gmail.com>
2025-09-14 15:44:54 +0200

    added a sphere collider and kart controller script (needs to be done)

---
+++ EsercizioKart/Assets/Scripts/KartController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class KartController : MonoBehaviour
+{
+    //statistiche del kart
+    [SerializeField] public float acceleration;
+    [SerializeField] public float deceleration;
+    [SerializeField] public float steeringSpeed = 80f;
+    [SerializeField] public float maxSpeed = 30f;
+    [SerializeField] public float driftMaxSpeed;
+    [SerializeField] public float driftMaxSteeringSpeed;
+
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
