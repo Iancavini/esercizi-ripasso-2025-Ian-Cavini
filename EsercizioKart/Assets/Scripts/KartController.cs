0585a71f48d4fe089162d9c746f8a868710390df
Ian Cavini <iancavinipinzon@gmail.com>
2025-09-20 15:57:35 +0200

    Fixed again the kart controls and added the drifting mechanic

--- EsercizioKart/Assets/Scripts/KartController.cs
+++ EsercizioKart/Assets/Scripts/KartController.cs
@@ -1,26 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class KartController : MonoBehaviour
 {
     //statistiche del kart
-    [SerializeField] public float _speed;
-
-    [SerializeField] public float _acceleration = 40f;
-    [SerializeField] public float _maxSpeed = 50f;
+    [SerializeField] public float _acceleration = 30f;
     //[SerializeField] public float _deceleration;
-
-    [SerializeField] public float _steeringSpeed = 80f;
+    [SerializeField] public float _steeringSpeed = 40f;
+    [SerializeField] public float _speed;
     //[SerializeField] public float _driftMaxSpeed;
     //[SerializeField] public float _driftMaxSteeringSpeed;
 
     [SerializeField] private SphereCollider _kartSphere;
     [SerializeField] private Rigidbody _kartRigidbody;
-
-    private float _inputSpeed;
     private float _horizontalInput;
 
     private float currentSpeed;
@@ -29,8 +22,8 @@ public class KartController : MonoBehaviour
     [SerializeField] public Transform _kartModel;
     public float gravity = 10f;
 
-    private bool _drifting = false;
     private int _driftDirection;
+    private bool _drifting;
 
     private void Awake()
     {
@@ -39,66 +32,60 @@ public class KartController : MonoBehaviour
 
     void Update()
     {
-        //Il modello del kart segue il suo collider
-        transform.position = _kartRigidbody.transform.position;
+        transform.position = _kartRigidbody.transform.position - new Vector3(0, .2f, 0);
 
-        //Tasto per accelerare
-        if (Input.GetKeyDown(KeyCode.Z))
+        //acceleratore
+        if (Input.GetKey(KeyCode.Z))
         {
-            _inputSpeed = 1;
-        }//if
-        else if (Input.GetKeyUp(KeyCode.Z))
+            _speed = _acceleration;
+        }
+        else
         {
-            _inputSpeed = 0;
+            _speed = 0;
         }
 
-        //Tasto per frenare e retromarcia
-        if (Input.GetKeyDown(KeyCode.X))
+        //sterzo
+        if (Input.GetAxis("Horizontal") != 0 && _speed != 0)
         {
-            _inputSpeed = -1;
-        }//if
-        else if (Input.GetKeyUp(KeyCode.X))
-        {
-            _inputSpeed = 0;
+            int dir = Input.GetAxis("Horizontal") > 0 ? 1 : -1;
+            float amount = Mathf.Abs((Input.GetAxis("Horizontal")));
+            Steer(dir, amount);
         }
-        
-        _speed = _inputSpeed * _acceleration;
 
-        _horizontalInput = Input.GetAxis("Horizontal");
-
-        //Tasto per sterzare
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, _horizontalInput * _steeringSpeed * Time.deltaTime * _inputSpeed, 0f));
-
-        //Tasto per derapare
-        if (Input.GetKey(KeyCode.C) && !_drifting && Input.GetAxis("Horizontal") != 0 )
+        //drifting
+        if (Input.GetKeyDown(KeyCode.C) && !_drifting && Input.GetAxis("Horizontal") != 0)
         {
             _drifting = true;
             _driftDirection = Input.GetAxis("Horizontal") > 0 ? 1 : -1;
-            
-        }//if
+        }
 
         if (_drifting)
         {
-            //float control = (_driftDirection == 1) ? ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 0, 2) : ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 2, 0);
+            float control = (_driftDirection == 1) ? ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 0, 2) : ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 2, 0);
+            Steer(_driftDirection, 1 * control);
         }
 
-        if (Input.GetKeyUp(KeyCode.C))
+        if (Input.GetKeyUp(KeyCode.C) && _drifting)
         {
             _drifting = false;
-        }//if
+        }
 
-        currentSpeed = _speed;
+        currentSpeed = Mathf.SmoothStep(currentSpeed, _speed, Time.deltaTime * 12f); _speed = 0f;
         currentRotate = Mathf.Lerp(currentRotate, rotate, Time.deltaTime * 4f); rotate = 0f;
+
 
     }//Update
 
     private void FixedUpdate()
     {
         //Kart acceleration
-        _kartRigidbody.AddForce(transform.forward * currentSpeed);
+        _kartRigidbody.AddForce(_kartModel.transform.forward * currentSpeed, ForceMode.Acceleration);
 
         //Gravity
         _kartRigidbody.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
+
+        //Steering
+        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y + currentRotate, 0), Time.deltaTime * 5f);
 
         RaycastHit hitOn;
         RaycastHit hitNear;
@@ -107,15 +94,31 @@ public class KartController : MonoBehaviour
         Physics.Raycast(transform.position, Vector3.down, out hitNear, 2.0f);
 
         //Normal rotation
-        //_kartModel.parent.up = Vector3.Lerp(_kartModel.parent.up, hitNear.normal, Time.deltaTime * 8.0f);
+        //_kartModel.parent.up = Vector3.Lerp(_kartModel.parent.up, hitNear.normal, Time.deltaTime * 8f);
         //_kartModel.parent.Rotate(0, transform.eulerAngles.y, 0);
 
+
     }//FixedUpdate
+
+    public void Steer(int direction, float amount)
+    {
+        rotate = (_steeringSpeed * direction) * amount;
+    }//Steer
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawLine(transform.position, transform.position - (transform.up * 2));
-    }//OnDrawGizmos
+    }
+
 
 }//KartController
+
+public static class ExtensionMethods
+{
+    public static float Remap(this float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
+
+}
